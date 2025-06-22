70decc85bf5fefc3f9dfa2ecdd3599055244caf0
Pablo <pablobarcellossoares@gmail.com>
2025-06-22 20:18:52 -0300

    Auto scraping

--- src/Interface/Cli.py
+++ src/Interface/Cli.py
@@ -8,3 +8,24 @@ if __name__ == "__main__":
 
     use_case = SavePost()
     register_student = SaveStudent()
+
+    def crawler_faculty():
+        use_case.update_all_memory()
+        sleep(10)
+        stick = 10
+
+        while True:
+            use_case.search_scraping_disciplines()
+            sleep(3)
+            use_case.search_scraping_posts()
+            sleep(3)
+
+            stick += 1
+            if stick >= 60:
+                stick = 0
+                use_case.update_all_memory()
+
+            sleep(60)
+
+    threading.Thread(target=crawler_faculty, daemon=True).start()
+
