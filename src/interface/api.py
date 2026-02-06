0f35ef37ffea0aef19ae9056dcf8d7d6d6203304
Pablo Barcellos Soares <74993616+PabloBarcellos-0522@users.noreply.github.com>
2026-02-06 10:35:17 -0300

    Port environment

--- src/interface/api.py
+++ src/interface/api.py
@@ -406,5 +406,6 @@ def delete_student(
 
 
 # --- Main entry point for uvicorn ---
+load_dotenv()
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api:app", host="0.0.0.0", port=os.getenv('PORT'), reload=True)
